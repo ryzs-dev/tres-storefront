1530bbb8fbf242d6a2c238d9a0cea658dfd18d25
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-06-04 10:51:29 +0800

    fix: redesign maintenance page

--- src/app/[countryCode]/maintenance/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Maintenance() {
-  return (
-    <main className="h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">🚧 Site under maintenance</h1>
-    </main>
-  )
-}
