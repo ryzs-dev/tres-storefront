e1a031eb4c100b6165cfe2df10dd73108c7adc8d
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-06-04 10:46:10 +0800

    chore: add and reroute maintenance page

---
+++ src/app/[countryCode]/maintenance/page.tsx
@@ -0,0 +1,7 @@
+export default function Maintenance() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">🚧 Site under maintenance</h1>
+    </main>
+  )
+}
