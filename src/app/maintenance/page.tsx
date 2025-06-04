d9cbe21ced84cf3ebbe2000268a6b51e7bd7598f
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-06-04 10:55:10 +0800

    fix: flex column maintenance page

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,6 +1,6 @@
 export default function Maintenance() {
   return (
-    <main className="h-screen flex items-center justify-center">
+    <main className="h-screen flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold font-urw">Site under maintenance.</h1>
       <h2 className="text-lg font-semibold font-urw">
         Sorry for the inconvenience
