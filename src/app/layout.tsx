688be07fd116c8cac240d53d510d38cb8f6822c4
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-10-03 17:32:31 +0800

    feature: Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -10,12 +11,11 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
+      <SpeedInsights />
       <body>
-        <main className="relative">
-          {props.children}
-          <SpeedInsights />
-        </main>
+        <main className="relative">{props.children}</main>
       </body>
+      <Analytics />
     </html>
   )
 }
